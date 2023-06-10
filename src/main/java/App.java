d9ec2c226a7e47981319047bfd0fbab3f8865fa9
KakosikNikitosik <thegoodness28@gmail.com>
2023-06-10 13:22:38 +0300

    Finished version

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanTwo.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getKind());
+
+        Cat beanCatTwo = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCatTwo.getKind());
+
+        System.out.println(bean.equals(beanTwo));
+        System.out.println(beanCat.equals(beanCatTwo));
     }
 }
