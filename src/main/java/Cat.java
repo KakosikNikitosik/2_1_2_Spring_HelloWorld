524540b4f6a45ba17e48fb417522b0de7a80e529
KakosikNikitosik <thegoodness28@gmail.com>
2023-06-10 13:23:35 +0300

    Finished version

---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+
+    private String kind;
+
+    public Cat() {
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+}
